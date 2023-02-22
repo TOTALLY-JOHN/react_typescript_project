a9187667e0120c31552b4dc2e733bcd4d9bd1041
jihwan.jeong <wnaks5945@gmail.com>
2023-02-22 17:04:15 +0900

    react first commit

---
+++ memo-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorldComponent from './component/HelloWorldComponent';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorldComponent />      
+    </div>
+  );
+}
+
+export default App;
---
+++ memo-app/src/component/HelloWorldComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HelloWorldComponent() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default HelloWorldComponent;
