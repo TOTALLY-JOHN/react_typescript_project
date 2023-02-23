3c7bc6ed10efa9f986ac0620e7dd409eb26a29da
jihwan.jeong <wnaks5945@gmail.com>
2023-02-23 16:11:58 +0900

    react state updates

--- memo-app/src/component/HelloWorldComponent.js
+++ memo-app/src/component/HelloWorldComponent.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function HelloWorldComponent(props, {job = 'Developer'}) {
+function HelloWorldComponent() {
+  const [userInfo, setUserInfo] = useState({});
+  const [submittedUserResult, setSubmittedUserResult] = useState();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    alert("Successfully submitted!");
+    setSubmittedUserResult(
+      <div className="submittedUserResult">
+        <p>Last Name: {userInfo.lastName}</p>
+        <p>First Name: {userInfo.firstName}</p>
+        <p>Job: {userInfo.job}</p>
+      </div>
+    );
+  }
+
+  const showInputValue = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value
+    });
+  }
+
   return (
     <div>
-      <h1>Information: {props.firstName} {props.lastName}({props.age}), Job: {job}</h1>
+      <h1>Form Submission</h1>
+      <form onSubmit={submitForm} autoComplete='off'>
+        Last Name: <input name="lastName" onChange={showInputValue} /> <br /><br />
+        First Name: <input name="firstName" onChange={showInputValue} /> <br /><br />
+        Job: <input name="job" onChange={showInputValue} /><br /><br /><br />
+        <button type="submit">Submit</button>
+      </form>
+      <br />
+      {submittedUserResult}
     </div>
   )
 }
