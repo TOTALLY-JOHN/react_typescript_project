7492c04f70f416ac1818555d51ef32a8fdd4b88e
jihwan.jeong <wnaks5945@gmail.com>
2023-02-23 16:12:03 +0900

    state updates

--- memo-app/src/App.js
+++ memo-app/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import HelloWorldComponent from './component/HelloWorldComponent';
-import HelloWorldCover from './component/HelloWorldCover';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <HelloWorldCover>
-        <HelloWorldComponent firstName="Andrew" lastName="Brian" age="30"/>
-      </HelloWorldCover>
+      <HelloWorldComponent firstName="Andrew" lastName="Brian" age="30"/>
     </div>
   );
 }
